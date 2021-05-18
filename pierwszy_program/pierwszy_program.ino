6ef9034bb234a6259ca959068f4529d505724d2d
Marcin521 <marcinkondo134@gmail.com>
2021-05-18 09:55:08 +0200

    send the finished project

--- pierwszy_program/pierwszy_program.ino
+++ pierwszy_program/pierwszy_program.ino
@@ -1,9 +1,20 @@
+/*
+Szkic Arduino flashing LED
+autor: Marcin Kondoł; date 16-05-2021
+*/
 void setup() {
-  // put your setup code here, to run once:
-
+  // put your setup code here, to run once: 
+ 
+  pinMode(13, OUTPUT); //ustawienie wyjścia
+   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  pinMode(13, HIGH); //włącza diode
+  delay(1000); // wstrzymuje działanie na sekundę
+  pinMode(13, LOW); //wyłącza diode
+  delay(1000); // wstrzymuje działanie na sekundę
+  
 }
