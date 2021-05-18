8dbce30f8779ed791d01ae782353fa53b2567d5f
Marcin521 <marcinkondo134@gmail.com>
2021-05-18 15:02:05 +0200

    cleaning

---
+++ przycisk/przycisk.ino
@@ -0,0 +1,20 @@
+/*
+ button program
+*/
+ #define LED 12
+ #define BUTTON 7
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+  //declares elements 
+  pinMode(BUTTON, INPUT); //set input
+  pinMode(LED, OUTPUT); //set output
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+}
