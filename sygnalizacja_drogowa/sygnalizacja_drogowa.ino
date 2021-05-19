ac77623fb385bdeb411078f4ce18e0f223c95599
Marcin521 <marcinkondo134@gmail.com>
2021-05-19 08:40:33 +0200

    define elements

--- sygnalizacja_drogowa/sygnalizacja_drogowa.ino
+++ sygnalizacja_drogowa/sygnalizacja_drogowa.ino
@@ -1,6 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
 
+  #define LED1  //red
+  #define LED2 //red
+
+  #define LED3  //yelow
+  #define LED4  //yelow
+
+  #define LED5  //green
+  #define LED6  //green
+
+  #define BUTTON1
+  #define BUTTON2   
+  
 }
 
 void loop() {
