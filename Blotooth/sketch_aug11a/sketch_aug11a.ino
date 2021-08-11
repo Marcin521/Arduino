d1e3e578f5735d31cbf3cba27ef53ff42778e2de
Marcin521 <marcinkondo134@gmail.com>
2021-08-11 15:05:55 +0200

    add new project

---
+++ Blotooth/sketch_aug11a/sketch_aug11a.ino
@@ -0,0 +1,23 @@
+int state;
+int led = 13;
+
+void setup() {
+  Serial1.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  state=Serial1.read();
+  Serial.print(state);
+  Serial.print("\n");
+  delay(200);
+  
+  if(state == '0'){
+     digitalWrite(led, HIGH); 
+        delay(1000);
+    }
+    else 
+  digitalWrite(led, LOW);    
+  delay(100);             
+  
+}
