a039062531da0fae335f2522c714782ae6c7dc79
Marcin521 <73833615+Marcin521@users.noreply.github.com>
2022-03-14 16:31:38 +0100

    blinking problem wos solwing

--- pierwsze_program/pierwszy_program.ino
+++ pierwsze_program/pierwszy_program.ino
@@ -6,15 +6,31 @@ void setup() {
   // put your setup code here, to run once: 
  
   pinMode(13, OUTPUT); //ustawienie wyjścia
+  pinMode(10, OUTPUT); //ustawienie wyjścia
+
    
+  pinMode(1, OUTPUT); //ustawienie wyjścia
+  pinMode(0, OUTPUT); //ustawienie wyjścia
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+while(1)
+{
+  digitalWrite(10, HIGH); //Włączenie diody /włącza diode
+  digitalWrite(13, HIGH);
+  delay(1000); // wstrzymuje działanie na sekundę
+  digitalWrite(13, LOW);
+  delay(1000); // wstrzymuje działanie na sekundę
 
-  pinMode(13, HIGH); //włącza diode
+
+  digitalWrite(1, HIGH); //Włączenie diody /włącza diode
+  digitalWrite(0, HIGH);
   delay(1000); // wstrzymuje działanie na sekundę
-  pinMode(13, LOW); //wyłącza diode
+  digitalWrite(0, LOW);
   delay(1000); // wstrzymuje działanie na sekundę
+  }
+
   
 }
